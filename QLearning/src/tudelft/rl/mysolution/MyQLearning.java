c795e0f416508ef0fcf509c1b71ce63b24414c1e
Cesar <cesar@debijlesmeester.nl>
2020-02-23 21:00:52 +0100

    part 5

--- QLearning/src/tudelft/rl/mysolution/MyQLearning.java
+++ QLearning/src/tudelft/rl/mysolution/MyQLearning.java
@@ -8,10 +8,18 @@ import tudelft.rl.State;
 
 public class MyQLearning extends QLearning {
 
+	public double alfa = 0.7;
+	public double gamma = 0.9;
+	public double epsilon = 0.1;
+
 	@Override
 	public void updateQ(State s, Action a, double r, State s_next, ArrayList<Action> possibleActions, double alfa, double gamma) {
-		// TODO Auto-generated method stub
-		
+		double maxval = Integer.MIN_VALUE;
+		for (Action act : possibleActions) {
+			maxval = Double.max(maxval, getQ(s_next, act));
+		}
+		double val = getQ(s, a) + alfa * (r + gamma * maxval - getQ(s, a));
+		setQ(s, a, val);
 	}
 
 }
