f7c29434af876a8fb5deca88e5e418dcf7bc7b66
jhagspiel <j.hagspiel@student.tudelft.nl>
2020-03-16 23:18:02 +0100

    3

--- ACO/src/AntColonyOptimization.java
+++ ACO/src/AntColonyOptimization.java
@@ -74,14 +74,14 @@ public class AntColonyOptimization {
      */
     public static void main(String[] args) throws FileNotFoundException {
     	//parameters
-    	int gen = 1000;
-        int noGen = 1000;
+    	int gen = 100;
+        int noGen = 100;
         double Q = 1600;
         double evap = 0.025;
         
         //construct the optimization objects
-        Maze maze = Maze.createMaze("./data/easy maze.txt");
-        PathSpecification spec = PathSpecification.readCoordinates("./data/easy coordinates.txt");
+        Maze maze = Maze.createMaze("./data/hard maze.txt");
+        PathSpecification spec = PathSpecification.readCoordinates("./data/hard coordinates.txt");
         AntColonyOptimization aco = new AntColonyOptimization(maze, gen, noGen, Q, evap);
         
         //save starting time
@@ -96,7 +96,7 @@ public class AntColonyOptimization {
         System.out.println(shortestRoute);
         
         //save solution
-        shortestRoute.writeToFile("./data/easy_solution.txt");
+        shortestRoute.writeToFile("./data/hard_solution.txt");
         
         //print route size
         System.out.println("Route size: " + shortestRoute.size());
