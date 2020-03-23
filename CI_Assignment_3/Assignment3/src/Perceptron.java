f2d840be807723a34255f39b2b5bacf7f0cfe494
jhagspiel <j.hagspiel@student.tudelft.nl>
2020-03-23 17:08:49 +0100

    first try

--- CI_Assignment_3/Assignment3/src/Perceptron.java
+++ CI_Assignment_3/Assignment3/src/Perceptron.java
@@ -1,2 +1,16 @@
 public class Perceptron {
+    double[] weightArray;
+
+    public int calc(double[] inputArray)
+    {
+        assert inputArray.length == this.weightArray.length;
+
+        int result = 0;
+        double tempResult = 0;
+
+        for (int i = 0; i < weightArray.length; i++) {
+            tempResult = weightArray[i]*inputArray[i];
+        }
+
+    }
 }
