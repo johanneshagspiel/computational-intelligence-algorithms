907261fdf09dd1f432838e9e08e0e14d80cf3c96
jhagspiel <j.hagspiel@student.tudelft.nl>
2020-03-25 15:34:29 +0100

    Finalized perceptron + added or example

--- CI_Assignment_3/Assignment3/src/Main.java
+++ CI_Assignment_3/Assignment3/src/Main.java
@@ -1,6 +1,30 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int[][] inputArray = new int[4][2];
+        inputArray[0][0] = 0;
+        inputArray[1][0] = 0;
+        inputArray[2][0] = 1;
+        inputArray[3][0] = 1;
+
+        inputArray[0][1] = 0;
+        inputArray[1][1] = 1;
+        inputArray[2][1] = 0;
+        inputArray[3][1] = 1;
+
+        int[] desiredResultArray = new int[4];
+        desiredResultArray[0] = 0;
+        desiredResultArray[1] = 0;
+        desiredResultArray[2] = 0;
+        desiredResultArray[3] = 1;
+
+        int epoch = 5;
+        double alpha = 0.1;
+
+	    Perceptron test = new Perceptron(inputArray[0].length);
+
+	    test.run(inputArray, desiredResultArray, epoch, alpha);
+
     }
 }
--- CI_Assignment_3/Assignment3/src/Perceptron.java
+++ CI_Assignment_3/Assignment3/src/Perceptron.java
@@ -1,15 +1,75 @@
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Perceptron {
     double[] weightArray;
+    double threshold;
 
-    public int calc(double[] inputArray)
+    Perceptron(int size)
+    {
+        weightArray = new double[size];
+        for (int i = 0; i < size; i++) {
+            weightArray[i] = ThreadLocalRandom.current().nextDouble(-0.5, 0.5);
+        }
+
+        threshold = ThreadLocalRandom.current().nextDouble(-0.5, 0.5);
+    }
+
+    public int activation(int[] inputArray)
     {
         assert inputArray.length == this.weightArray.length;
 
-        int result = 0;
         double tempResult = 0;
 
         for (int i = 0; i < weightArray.length; i++) {
-            tempResult = weightArray[i]*inputArray[i];
+            tempResult += weightArray[i]*inputArray[i];
+        }
+
+        tempResult -= this.threshold;
+
+        if (tempResult >= 0)
+        {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public void weightTraining(int[] inputArray, int result, int desiredResult, double alpha)
+    {
+        assert inputArray.length == this.weightArray.length;
+
+        int error = desiredResult - result;
+
+        if (error == 0)
+        {
+            return;
+        }
+
+        for (int i = 0; i < this.weightArray.length; i++) {
+            weightArray[i] += alpha*error*inputArray[i];
+        }
+    }
+
+    public void run(int[][] inputArray, int[] desiredResultArray, int epoch, double alpha)
+    {
+        assert this.weightArray.length == desiredResultArray.length;
+        assert this.weightArray.length == inputArray[0].length;
+
+        for (int i = 0; i < epoch; i++) {
+
+            for (int iteration = 0; iteration < inputArray.length; iteration++) {
+
+                int result = activation(inputArray[iteration]);
+                int error = desiredResultArray[iteration] - result;
+
+                weightTraining(inputArray[iteration], result, desiredResultArray[iteration], alpha);
+
+                System.out.println("Epoch " + (i+1) + ", Iteration " + (iteration+1) + " Error " + error);
+
+            }
+
         }
 
     }
