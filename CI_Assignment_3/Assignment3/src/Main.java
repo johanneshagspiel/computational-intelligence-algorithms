10e3598b29695ab0d443c8c26bd37fcec5c6e702
Cesar <cesar@debijlesmeester.nl>
2020-03-27 21:40:40 +0100

    update & comments

--- CI_Assignment_3/Assignment3/src/Main.java
+++ CI_Assignment_3/Assignment3/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //here, we declare the input and desired output of the algorithm
         double[][] inputArray = new double[4][2];
         inputArray[0][0] = 0;
         inputArray[1][0] = 0;
@@ -16,31 +17,22 @@ public class Main {
         inputArray[2][1] = 0;
         inputArray[3][1] = 1;
 
-        int[] desiredResultArray = new int[4];
-        desiredResultArray[0] = 0;
-        desiredResultArray[1] = 1;
-        desiredResultArray[2] = 1;
-        desiredResultArray[3] = 0;
+        int[][] desiredResultArray = new int[4][1];
+        desiredResultArray[0][0] = 0;
+        desiredResultArray[1][0] = 1;
+        desiredResultArray[2][0] = 1;
+        desiredResultArray[3][0] = 1;
 
-        int epoch = 100;
-        double alpha = 0.2;
+        //we also set the hyperparameters
+        int epoch = 50;
+        double alpha = 0.7;
+
+        //we now instantiate the object with the right data, and run it
 
 //	    Perceptron test = new Perceptron(inputArray[0].length, new StepFunction());
 //        test.run(inputArray, desiredResultArray, epoch, alpha);
-        MultiLayer ml = new MultiLayer(1,2,2,1, new SigmoidFunction());
-
-        for (int i = 0; i < epoch; i++) {
-            double epocherror = 0;
-            for (int j = 0; j < inputArray.length; j++) {
-                double[][] res = ml.process(inputArray[j]);
-                double totalerror = 0;
-                for (int k = 0; k < res[res.length - 1].length; k++) totalerror += Math.abs(res[res.length - 1][k] - desiredResultArray[k]);
-                System.out.println("Average error of iteration " + j + ": " + totalerror/res.length);
-                epocherror += totalerror/res.length;
-                ml.backPropagate(res, desiredResultArray, alpha);
-            }
-            System.out.println("Total error of epoch " + i + ": " + epocherror/inputArray.length);
-        }
+        MultiLayer ml = new MultiLayer(1, 2, inputArray[0].length, desiredResultArray[0].length, new SigmoidFunction());
+        ml.run(epoch, alpha, inputArray, desiredResultArray);
 
     }
 }
--- CI_Assignment_3/Assignment3/src/MultiLayer.java
+++ CI_Assignment_3/Assignment3/src/MultiLayer.java
@@ -1,6 +1,14 @@
 import ActivationFunctions.ActivationFunction;
 
+import java.util.Arrays;
+
 public class MultiLayer {
+
+    //We use a lot of arrays of arrays, which are usually not 2d-arrays.
+    //These arrays generally have the shape our MLP would have in a drawing, though sometimes
+    //without the input (since that is not technically part of the MLP).
+    //This is the most convenient format for storing perceptrons and values corresponding to them,
+    //since it keeps the layered structure and semi-irregular (i.e. inputsize != hiddenlayersize != outputsizze) shape.
 
     Perceptron[][] neurons;
     int hiddenlayers;
@@ -15,7 +23,7 @@ public class MultiLayer {
         inputs = i;
         outputs = o;
         activationFunction = a;
-        neurons = new Perceptron[hl + 1][];
+        neurons = new Perceptron[hl + 1][]; //we need to store all hidden layers and the output layer. The input layer is given.
         neurons[0] = new Perceptron[npl];
         for (int j = 0; j < npl; j++) neurons[0][j] = new Perceptron(i, a); //all inputs feed into the first hidden layer, so this layer needs #inputs inputs for each perceptron
         for (int j = 1; j < hl; j++) {
@@ -31,10 +39,14 @@ public class MultiLayer {
         res[0] = input;
         for (int i = 0; i < hiddenlayers; i++) {
             res[i+1] = new double[neuronsperlayer];
-            for (int j = 0; j < neuronsperlayer; j++) res[i+1][j] = neurons[i][j].activation(res[i]); //for each hidden layer, calculate new result from prev result
+            for (int j = 0; j < neuronsperlayer; j++) {
+                res[i+1][j] = neurons[i][j].activation(res[i]); //for each hidden layer, calculate new result from prev result
+            }
         }
         res[hiddenlayers+1] = new double[outputs];
-        for (int i = 0; i < outputs; i++) res[hiddenlayers+1][i] = neurons[hiddenlayers][i].activation(res[hiddenlayers]); //use output of last hidden layer to find output array
+        for (int i = 0; i < outputs; i++) {
+            res[hiddenlayers+1][i] = neurons[hiddenlayers][i].activation(res[hiddenlayers]); //use output of last hidden layer to find output array
+        }
         return res;
     }
 
@@ -44,11 +56,12 @@ public class MultiLayer {
         assert results[0].length == inputs;
         for (int i = 1; i < hiddenlayers+1; i++) assert results[i].length == neuronsperlayer;
         assert labels.length == outputs;
-        double[][] prevs = new double[hiddenlayers + 1][];
+
+        double[][] prevs = new double[hiddenlayers + 1][]; //store all chained values
         prevs[hiddenlayers] = new double[outputs];
         for (int i = 0; i < outputs; i++) {
             Perceptron current = neurons[hiddenlayers][i];
-            prevs[hiddenlayers][i] = (labels[i] - results[hiddenlayers+1][i]) * activationFunction.getDerivative(current.preprocess(results[hiddenlayers]), current.threshold);
+            prevs[hiddenlayers][i] = (labels[i] - ((int) (results[hiddenlayers+1][i] + 0.5))) * activationFunction.getDerivative(current.preprocess(results[hiddenlayers]), current.threshold);
         }
         for (int i = hiddenlayers - 1; i >= 0; i--) {
             prevs[i] = new double[neuronsperlayer];
@@ -64,5 +77,25 @@ public class MultiLayer {
                 neurons[i][j].threshold -= alpha * prevs[i][j];
             }
         }
+    }
+
+    public void run(int epoch, double alpha, double[][] inputArray, int[][] desiredResultArray) {
+        double firsterror = -1;
+        for (int i = 0; i < epoch; i++) {
+            double epocherror = 0;
+            for (int j = 0; j < inputArray.length; j++) {
+                double[][] res = process(inputArray[j]);
+                double totalerror = 0;
+//                System.out.println(Arrays.toString(res[res.length-1]));
+//                System.out.println(Arrays.toString(desiredResultArray[j]));
+                for (int k = 0; k < res[res.length - 1].length; k++) totalerror += Math.abs(((int) (res[res.length - 1][k] + 0.5)) - desiredResultArray[j][k]);
+                System.out.println("Average error of iteration " + j + ": " + totalerror/res[res.length - 1].length);
+                epocherror += totalerror/res[res.length - 1].length;
+                backPropagate(res, desiredResultArray[j], alpha);
+            }
+            System.out.println("Total error of epoch " + i + ": " + epocherror/inputArray.length);
+            if (i == 0) firsterror = epocherror/inputArray.length;
+        }
+        System.out.println("We started at " + firsterror);
     }
 }
